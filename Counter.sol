f8cc5a74b7e0dd3625fbb4f7e17e79e6da1f22cb
wangyue <your.new.email@example.com>
2025-07-23 12:23:53 +0800

    first commit

---
+++ Counter.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 private count;
+
+    constructor() {
+        count = 0;
+    }
+
+    function increment() public {
+        count += 1;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
